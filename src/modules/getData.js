82b97aa6653e08b3a0aef1cbba996a3338a589a5
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-08-14 10:00:16 +0300

    add библиотека Notify

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,7 +1,33 @@
+import Notify from 'simple-notify'
+
 const getData = () => {
     return fetch('https://ozonbd-46860-default-rtdb.firebaseio.com/goods.json')
     .then((response) => {
-        return response.json()
+        // new Notify({
+        //     status: 'success',
+        //     title: 'Notify Title',
+        //     text: 'notify text',
+        //     effect: 'slide',
+        //     type: 3,
+        //     autoclose: true
+        // })
+        if (response.ok) {
+            return response.json()
+        } else {
+            throw new Error('Ошибка запроса')
+        }
+        
+        // return response.json()
+    })
+    .catch(Error => {  
+        new Notify({
+            status: 'error',
+            title: 'Ошибка!',
+            text: Error.message,
+            effect: 'slide',
+            type: 3,
+            autoclose: true
+        });
     })
         
 }
