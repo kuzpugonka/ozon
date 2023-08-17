bb8b812ca3cc73203b4c9c5ab98e2af2fda6f3ed
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-08-17 18:00:04 +0300

    add фильтр по цене, акции и поиску

--- src/modules/filters.js
+++ src/modules/filters.js
@@ -1,13 +1,29 @@
-export const searchFilter = (goods, value) => {
+export const categoryFilter = (goods, value) => {
   // console.log(goods);
   return goods.filter((goodsItem) => {
-    return goodsItem.title.toLowerCase().includes(value.toLowerCase());
+    return goodsItem.category === value;
   });
 };
 
-export const categoryFilter = (goods, value) => {
-    // console.log(goods);
-    return goods.filter((goodsItem) => {
-      return goodsItem.category === value
-    });
-  };
+export const funcFilter = (goods, minValue, maxValue, checkValue, searchValue) => {
+  // console.log(goods);
+  // console.log(minValue);
+  // console.log(maxValue);
+  console.log(checkValue);
+
+  return goods.filter((goodsItem) => {
+    // console.log(goodsItem);
+    // return goodsItem.price >= parseInt(minValue) && goodsItem.price <= parseInt(maxValue)
+    // console.log(goodsItem);
+
+    const isMin = minValue.trim()
+      ? goodsItem.price >= parseInt(minValue.trim())
+      : true;
+    const isMax = maxValue.trim()
+      ? goodsItem.price <= parseInt(maxValue.trim())
+      : true;
+    const isSale = checkValue ? goodsItem.sale : true;
+
+    return isMin && isMax && isSale && goodsItem.title.toLowerCase().includes(searchValue.toLowerCase());
+  });
+};
--- src/modules/search.js
+++ src/modules/search.js
@@ -1,18 +1,50 @@
 import getData from "./getData";
 import renderGoods from "./renderGoods";
-import { searchFilter } from "./filters";
+import { funcFilter } from "./filters";
 import { debounce } from "./helper";
 
 const search = () => {
     const searchInput = document.querySelector(".search-wrapper_input");
+    const minInp = document.getElementById('min')
+    const maxInp = document.getElementById('max')
+    const checkInp = document.getElementById('discount-checkbox')
+    const checkMark = document.querySelector('.filter-check_checkmark')
 
-    const debounceSearch = debounce((event) => {
+
+    const debounceFunc = debounce((min = '', max = '', checkValue = false, searchValue = '') => {
+        // console.log(min);
+        // console.log(max);
         getData().then((data) => {
-            renderGoods(searchFilter(data, event.target.value))
+            renderGoods(funcFilter(data, min, max, checkValue, searchValue))
         }) 
-    }, 2000)
+    })
 
-    searchInput.addEventListener("input", debounceSearch);
+    searchInput.addEventListener("input", () => {
+        debounceFunc(minInp.value, maxInp.value, checkInp.value, searchInput.value)
+
+    });
+
+    minInp.addEventListener('input', () => {
+        // console.log('min');
+        debounceFunc(minInp.value, maxInp.value, checkInp.value, searchInput.value)
+    })
+
+    maxInp.addEventListener('input', () => {
+        // console.log('max');
+        debounceFunc(minInp.value, maxInp.value, checkInp.value, searchInput.value)
+
+    })
+
+    checkInp.addEventListener('input', () => {
+        // console.log(checkInp.checked);
+        if (checkInp.checked) {
+            checkMark.classList.add('checked')
+        } else {
+            checkMark.classList.remove('checked')
+        }
+        debounceFunc(minInp.value, maxInp.value, checkInp.value, searchInput.value)
+
+    })
 };
 
 export default search;
